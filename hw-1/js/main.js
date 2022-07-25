093485f9d77aa90ca3e05b9e61490a7580aed7f4
Martozhyna <mybonanzaluck@gmail.com>
2022-07-25 13:47:35 +0300

    Initial commit

---
+++ hw-1/js/main.js
@@ -0,0 +1,85 @@
+let a = 'hello';
+console.log(a);
+// alert(a);
+document.write(a);
+
+let b = 'owu';
+console.log(b);
+// alert(b);
+document.write(b);
+
+let c = 'com';
+console.log(c);
+// alert(c);
+document.write(c);
+
+let d = 'ua';
+console.log(d);
+// alert(d);
+document.write(d);
+
+let e = 1;
+console.log(e);
+// alert(e);
+document.write(e);
+
+let f = 10;
+console.log(f);
+// alert(f);
+document.write(f);
+
+let g = -999;
+console.log(g);
+// alert(g);
+document.write(g);
+
+let h = 123;
+console.log(h);
+// alert(h);
+document.write(h);
+
+let i = 3.14;
+console.log(i);
+// alert(i);
+document.write(i);
+
+let j = 2.7;
+console.log(j);
+// alert(j);
+document.write(j);
+
+let k = 16;
+console.log(k);
+// alert(k);
+document.write(k);
+
+let l = true;
+console.log(l);
+// alert(l);
+document.write(l);
+
+let m = false;
+console.log(m);
+// alert(m);
+document.write(m);
+
+let firstName = 'Bida';
+let middleName = 'Marta';
+let lastName = 'Serhiivna';
+let space = ' ';
+let person = firstName + space + middleName + space + lastName;
+console.log(person)
+
+let n = 100;
+console.log(typeof n);
+let o = '100';
+console.log(typeof o);
+let p = true;
+console.log(typeof p);
+
+let name = prompt('What is your name?');
+console.log(name);
+let second = prompt('What is your last name?');
+console.log(second);
+let age = prompt('How old are you?');
+console.log(age);
